701553dbdbbc736a24b2973702799f686d86bc97
yuramorozov01 <yura_morozov01@mail.ru>
2022-02-08 20:12:05 +0300

    fix (back): fix deposit withdraw and deposit interest accrual

--- bank/src/deposit_app/utils.py
+++ bank/src/deposit_app/utils.py
@@ -4,17 +4,29 @@ from decimal import Decimal
 
 
 def deposit_withdraw(deposit_contract):
+    deposit_bank_account = deposit_contract.deposit_bank_account
+    main_bank_account = deposit_contract.main_bank_account
+
     transfer_money(
-        deposit_contract.deposit_bank_account,
-        deposit_contract.main_bank_account,
-        deposit_contract.deposit_bank_account.balance
+        deposit_bank_account,
+        main_bank_account,
+        deposit_bank_account.balance
     )
+
+    deposit_bank_account.activity_type = BankAccountActivityTypeChoices.PASSIVE
+    deposit_bank_account.save()
+    main_bank_account.save()
+
+    special_bank_account = deposit_contract.special_bank_account
+
     transfer_money(
-        deposit_contract.special_bank_account,
-        deposit_contract.main_bank_account,
+        special_bank_account,
+        main_bank_account,
         deposit_contract.deposit_amount
     )
-    deposit_contract.deposit_bank_account.activity_type = BankAccountActivityTypeChoices.PASSIVE
+
+    special_bank_account.save()
+    main_bank_account.save()
 
 
 def deposit_interest_accrual(deposit_contract):
@@ -22,8 +34,15 @@ def deposit_interest_accrual(deposit_contract):
     percent = deposit_contract.deposit_type.percent
     term = deposit_contract.deposit_type.deposit_term
     transfer_amount = total_balance * (Decimal(percent) / 100) / term
+
+    special_bank_account = deposit_contract.special_bank_account
+    deposit_bank_account = deposit_contract.deposit_bank_account
+
     transfer_money(
-        deposit_contract.special_bank_account,
-        deposit_contract.deposit_bank_account,
+        special_bank_account,
+        deposit_bank_account,
         transfer_amount
     )
+
+    special_bank_account.save()
+    deposit_bank_account.save()
