36bc940b82f29f9c04640f9645f2b40df43e4f0f
yuramorozov01 <yura_morozov01@mail.ru>
2022-02-16 21:36:38 +0300

    fix (back): fix calculation debt amount in case of not annuity payment

--- bank/src/credit_app/utils.py
+++ bank/src/credit_app/utils.py
@@ -6,12 +6,12 @@ from base_app.models import BankSettings
 from credit_app.models import CreditContract
 
 
-def calc_debt_amount(credit_contract, pay_off=False):
+def calc_debt_amount(bank_settings, credit_contract, pay_off=False):
     days_left = 1
     if pay_off:
         # Calculate number of days left until credit end date
         # +1 in `days_left` means including current day
-        bank_settings = BankSettings.objects.get_or_create()
+        # days_left` will be not equal to 1 only if current operation is pay off
         days_left = (credit_contract.ends_at - bank_settings.curr_bank_day).days + 1
 
     # Calculate debt amount
@@ -19,15 +19,25 @@ def calc_debt_amount(credit_contract, pay_off=False):
     percent = Decimal(credit_contract.credit_type.percent) / 100
     credit_term = credit_contract.credit_type.credit_term
 
-    debt_amount = credit_amount + (((credit_amount * percent) / credit_term) * days_left)
+    # Differential single debt amount
+    # In case of payoff operation, debt amount will be calculated by ...
+    # ... multiplying on left days (1 means last day) and addition with sum of credit
+    debt_amount = ((credit_amount * percent) / credit_term) * days_left
+
+    if pay_off:
+        debt_amount += credit_amount
+
+    # Annuity single debt amount
+    # In case of payoff operation, debt amount will be calculated by ...
+    # ... multiplying on left days (1 means last day)
     if credit_contract.credit_type.is_annuity_payment:
         debt_amount = (credit_amount * (1 + percent) / credit_term) * days_left
 
     return debt_amount
 
 
-def make_credit_payment(credit_contract, pay_off=False):
-    debt_amount = calc_debt_amount(credit_contract, pay_off=pay_off)
+def make_credit_payment(bank_settings, credit_contract, pay_off=False):
+    debt_amount = calc_debt_amount(bank_settings, credit_contract, pay_off=pay_off)
 
     # Transfer debt money from credit bank account to special fund bank account
     credit_bank_account = credit_contract.credit_bank_account
@@ -44,15 +54,15 @@ def make_credit_payment(credit_contract, pay_off=False):
     special_bank_account.refresh_from_db()
 
 
-def credit_payoff(credit_contract):
-    make_credit_payment(credit_contract, pay_off=True)
+def credit_payoff(bank_settings, credit_contract):
+    make_credit_payment(bank_settings, credit_contract, pay_off=True)
     credit_contract.is_ended = True
     credit_contract.save()
     credit_contract.refresh_from_db()
 
 
-def credit_interest_payment(credit_contract):
-    make_credit_payment(credit_contract, pay_off=False)
+def credit_interest_payment(bank_settings, credit_contract):
+    make_credit_payment(bank_settings, credit_contract, pay_off=False)
     credit_contract.save()
     credit_contract.refresh_from_db()
 
@@ -74,9 +84,9 @@ def credit_daily_recount(bank_settings):
     for credit_contract in credit_contracts:
         # Credit interest payment if today is credit day
         if (credit_contract.starts_at <= bank_settings.curr_bank_day) and \
-                (bank_settings.curr_bank_day <= credit_contract.ends_at):
-            credit_interest_payment(credit_contract)
+           (bank_settings.curr_bank_day < credit_contract.ends_at):
+            credit_interest_payment(bank_settings, credit_contract)
 
         # Pay off credit if today is the last day of credit
-        if bank_settings.curr_bank_day == credit_contract.ends_at:
-            credit_payoff(credit_contract)
+        elif bank_settings.curr_bank_day == credit_contract.ends_at:
+            credit_payoff(bank_settings, credit_contract)
