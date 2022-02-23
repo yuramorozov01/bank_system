3d2ced6f2a99ee40c039835e000e87de2b2111b1
yuramorozov01 <yura_morozov01@mail.ru>
2022-02-23 21:07:23 +0300

    feat (front): add balance function in ATM with receipt printing

--- bank/webclient/bank/src/app/atm/atm-balance-page/atm-balance-page.component.ts
+++ bank/webclient/bank/src/app/atm/atm-balance-page/atm-balance-page.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { IReceipt } from '../../shared/interfaces/utils.interfaces';
+
+import { AtmService } from '../../shared/services/atm/atm-service';
+import { MaterializeService } from '../../shared/services/utils/materialize.service';
+
 
 @Component({
   selector: 'app-atm-balance-page',
@@ -6,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atm-balance-page.component.css']
 })
 export class AtmBalancePageComponent implements OnInit {
+    @ViewChild('pdfReceipt', {static: false}) pdfReceipt: ElementRef;
 
-  constructor() { }
+	balanceReceipt: IReceipt;
 
-  ngOnInit(): void {
-  }
+	constructor(private router: Router,
+				private route: ActivatedRoute,
+				private atmService: AtmService) { }
 
+	ngOnInit(): void {
+        this.refreshBalance();
+	}
+
+    refreshBalance(): void {
+        this.route.params
+			.pipe(
+				switchMap(
+					(params: Params) => {
+                        return this.atmService.balance();
+					}
+				)
+			)
+			.subscribe(
+				(balanceReceipt: IReceipt) => {
+					if (balanceReceipt) {
+						this.balanceReceipt = balanceReceipt;
+					}
+				},
+                error => MaterializeService.toast(error.error),
+			);
+    }
+
+	printReceipt() {
+        this.atmService.printReceipt(this.pdfReceipt);
+	}
 }
---
+++ bank/webclient/bank/src/app/shared/services/atm/atm-service.ts
@@ -0,0 +1,44 @@
+import { Injectable, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { IReceipt } from '../../interfaces/utils.interfaces';
+import { FormGroup } from '@angular/forms';
+import { ParserService } from '../utils/parser.service';
+import { MaterializeService } from '../utils/materialize.service';
+
+import { jsPDF } from 'jspdf';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AtmService {
+	constructor(private http: HttpClient,
+                private parserService: ParserService) { }
+
+    balance(): Observable<IReceipt> {
+        return this.http.get<IReceipt>('/api/v1/bank_card/balance/');
+    }
+
+    withdraw(formGroup: FormGroup): Observable<IReceipt> {
+        const formData = this.parserService.getValuesFromFormGroup(formGroup);
+        return this.http.put<IReceipt>(`/api/v1/bank_card/withdraw/`, formData);
+    }
+
+    printReceipt(receiptElementRef: ElementRef): void {
+        const doc = new jsPDF();
+
+        const pdfReceipt = receiptElementRef.nativeElement;
+
+        doc.html(pdfReceipt.innerHTML, {
+            callback: function (doc) {
+                doc.save('balanceReceipt.pdf');
+                MaterializeService.toast({'Success': 'Receipt has been successfully saved!'});
+            },
+            margin: [5, 5, 5, 5],
+            windowWidth: 200,
+            width: 150,
+        });
+    }
+}
